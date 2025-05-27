c0b55c0786ae8a95da6f2a927df8827e31e96973
mirellacristina <mirela.pereira@estudante.ifms.edu.br>
2025-05-27 07:40:34 -0400

    Remover arquivos antigos e adicionar novos componentes e rota de todolist

---
+++ src/routes/todolist/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/stores';
+  
+    $: title = $page.data.title ?? 'App de Tarefas';
+  </script>
+  
+  <nav class="navbar bg-primary text-white px-4">
+    <h1>{title}</h1>
+  </nav>
+  
+  <slot />
+  
---
+++ src/routes/todolist/+page.js
@@ -0,0 +1,6 @@
+export function load() {
+    return {
+      title: 'Lista de Tarefas'
+    };
+  }
+  
